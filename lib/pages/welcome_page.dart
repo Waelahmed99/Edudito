d28488ede1945592f59ee8609088f939c489c2d7
Waelahmed99 <Waelahmed6599@gmail.com>
2021-02-19 01:21:53 +0200

    Creating assets | creating welcome_page

---
+++ lib/pages/welcome_page.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Text(
+              'Edudito',
+              style: TextStyle(
+                fontSize: 30,
+                color: Color(0xff07225E),
+              ),
+            ),
+            Image.asset('assets/welcome_icon.png'),
+            _buildWelcomeWidget(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildWelcomeWidget(context) {
+    return Container(
+      padding: EdgeInsets.only(left: 33, right: 16),
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: Color(0xff07225E),
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(30),
+          topLeft: Radius.circular(30),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 50),
+          Text(
+            'Welcome',
+            style: TextStyle(fontSize: 20, color: Colors.white),
+          ),
+          SizedBox(height: 24),
+          Text(
+            'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.',
+            style: TextStyle(fontSize: 14, color: Colors.white),
+          ),
+          SizedBox(height: 50),
+          Container(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  _buildContainerButton(
+                    text: 'Sign In',
+                    color: Color(0xffF8DE44),
+                    onTap: null,
+                  ),
+                  _buildContainerButton(
+                    text: 'Sign Up',
+                    color: Color(0xffffffff),
+                    onTap: null,
+                  ),
+                ],
+              )),
+          SizedBox(height: 40),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildContainerButton({String text, Function onTap, Color color}) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 42, vertical: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: color,
+        ),
+        child: Text(text),
+      ),
+    );
+  }
+}
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
+import 'pages/welcome_page.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    /// [todo] Fetch data and load login or home screen.
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Future.delayed(
+        Duration(seconds: 2),
+        () => Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(
+              builder: (context) => WelcomePage(),
+            ),
+            (Route<dynamic> route) => false),
+      );
+    });
     return Scaffold(
       body: Center(
         child: Text('Splash screen'),
