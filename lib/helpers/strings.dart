84e35b51891684d566a3a07d0ec6fdddc22794f5
Waelahmed99 <Waelahmed6599@gmail.com>
2021-02-19 16:45:08 +0200

    Creating Strings

---
+++ lib/helpers/strings.dart
@@ -0,0 +1,17 @@
+class Strings {
+  // main strings
+  final String appName = 'Edudito';
+
+  // Welcome|auth strings
+  final String welcomeMessage =
+      'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.';
+  final String signIn = 'Sign In';
+  final String signUp = 'Sign Up';
+  final String signInMessage = 'Welcome back';
+  final String signUpMessage = 'Let\'s start!';
+  final String email = 'Email';
+  final String password = 'Password';
+  final String name = 'Your name';
+  final String switchToSignUp = 'Don\'t have an account?';
+  final String switchtoSignIn = 'Already have an account?';
+}
