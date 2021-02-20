302e62e42328fdf9dfc8e1dcc5cafb03a709088a
Waelahmed99 <Waelahmed6599@gmail.com>
2021-02-21 00:55:29 +0200

    Configuring provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:Edudito/helpers/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'splash_screen.dart';
 
 Future<void> main() async {
@@ -8,14 +10,30 @@ Future<void> main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  ProviderModel provider;
+
+  @override
+  void initState() {
+    provider = ProviderModel();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(fontFamily: 'Poppins'),
-      routes: {
-        '/': (context) => SplashScreen(),
-      },
+    return ChangeNotifierProvider.value(
+      value: provider,
+      child: MaterialApp(
+        theme: ThemeData(fontFamily: 'Poppins'),
+        routes: {
+          '/': (context) => SplashScreen(),
+        },
+      ),
     );
   }
 }
