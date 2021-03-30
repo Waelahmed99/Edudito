78e901f6f5a2e111cc512ff99978ceb36f35b9bc
waelahmed99 <waelahme6599@gmail.com>
2021-03-30 22:27:28 +0200

    Creating material page

---
+++ lib/screens/material_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MyMaterialPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Material'),
+      ),
+    );
+  }
+}
