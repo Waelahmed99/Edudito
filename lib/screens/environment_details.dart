6eec20acc554791b1cfb5bc0b393ff3df182c08b
waelahmed99 <waelahme6599@gmail.com>
2021-03-30 22:00:05 +0200

    Implementing custom back button

--- lib/screens/environment_details.dart
+++ lib/screens/environment_details.dart
@@ -2,6 +2,7 @@ import 'package:Edudito/helpers/style_guide.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
 class EnvironmentDetails extends StatelessWidget {
   final QueryDocumentSnapshot snapshot;
@@ -12,7 +13,13 @@ class EnvironmentDetails extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Container(),
+        leading: GestureDetector(
+          onTap: () => Navigator.of(context).pop(),
+          child: Icon(
+            LineAwesomeIcons.angle_left,
+            color: StyleGuide.mainColor,
+          ),
+        ),
         backgroundColor: Colors.transparent,
         bottomOpacity: 0.0,
         elevation: 0.0,
