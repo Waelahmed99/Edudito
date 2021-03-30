4d4ef44e81b1f2d5be63818503cb8e330a52f036
waelahmed99 <waelahme6599@gmail.com>
2021-03-30 22:26:16 +0200

    Adding AppBar to ProgressPage

--- lib/screens/progress_page.dart
+++ lib/screens/progress_page.dart
@@ -1,11 +1,27 @@
+import 'package:Edudito/helpers/style_guide.dart';
 import 'package:flutter/material.dart';
 
 class ProgressPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: Container(),
+        backgroundColor: Colors.transparent,
+        bottomOpacity: 0.0,
+        elevation: 0.0,
+        centerTitle: true,
+        title: Text(
+          'Progress',
+          overflow: TextOverflow.ellipsis,
+          style: TextStyle(
+            color: StyleGuide.mainColor,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+      ),
       body: Center(
-        child: Text('Progress'),
+        child: Text('Plantracker'),
       ),
     );
   }
