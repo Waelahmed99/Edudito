16893106e110d3dc0297cdb36791fd8f4c8e302c
Ashour <mashour365@gmail.com>
2021-03-31 03:27:31 +0200

    Finalized project

--- lib/screens/plan_tracker.dart
+++ lib/screens/plan_tracker.dart
@@ -1,12 +1,9 @@
+import 'package:Edudito/screens/artest.dart';
 import 'package:flutter/material.dart';
 
 class PlanTrackerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Progress'),
-      ),
-    );
+    return HelloWorld();
   }
 }
