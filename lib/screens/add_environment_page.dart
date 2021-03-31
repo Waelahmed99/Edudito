f366bcbd8c5fc7ee3b659a3d278f14d15d004734
Waelahmed99 <waelahmed6599@gmail.com>
2021-03-31 02:56:28 +0200

    Adding categories

--- lib/screens/add_environment_page.dart
+++ lib/screens/add_environment_page.dart
@@ -1,8 +1,16 @@
+import 'package:Edudito/helpers/strings.dart';
 import 'package:Edudito/helpers/style_guide.dart';
 import 'package:flutter/material.dart';
 import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
-class AddEnvironmentPage extends StatelessWidget {
+class AddEnvironmentPage extends StatefulWidget {
+  @override
+  _AddEnvironmentPageState createState() => _AddEnvironmentPageState();
+}
+
+class _AddEnvironmentPageState extends State<AddEnvironmentPage> {
+  int _selectedCategory = -1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -83,6 +91,27 @@ class AddEnvironmentPage extends StatelessWidget {
                     ),
                   ),
                 ),
+                SizedBox(height: 12),
+                Text(
+                  'Category',
+                  style: TextStyle(
+                    color: StyleGuide.mainColor,
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: Strings.categories
+                        .asMap()
+                        .map((key, value) =>
+                            MapEntry(key, categoryItem(value, key)))
+                        .values
+                        .toList(),
+                  ),
+                ),
                 SizedBox(height: 22),
                 Container(
                   alignment: Alignment.topLeft,
@@ -97,6 +126,29 @@ class AddEnvironmentPage extends StatelessWidget {
                 SizedBox(height: 5),
               ],
             ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget categoryItem(String item, int idx) {
+    return GestureDetector(
+      onTap: () => setState(() => _selectedCategory = idx),
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 2, horizontal: 10),
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(22),
+          color: idx == _selectedCategory ? Colors.blue : null,
+          border: Border.all(
+              width: 1,
+              color: idx == _selectedCategory ? Colors.blue : Colors.blue),
+        ),
+        child: Text(
+          item,
+          style: TextStyle(
+            color: idx == _selectedCategory ? Colors.white : null,
           ),
         ),
       ),
